1313810ee84f2dd1d647bfc046575a66efa61376
lazarus0320 <astar5327z@gmail.com>
2023-02-02 23:41:31 +0900

    23/02/02

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useReducer } from 'react';
+import produce from 'immer';
+import CreateUser from './CreateUser';
+import UserList from './UserList';
+import useInputs from './hooks/useInputs';
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중..');
+  return users.filter((user) => user.active).length;
+}
+
+const initialState = {
+  inputs: {
+    username: '',
+    email: '',
+  },
+  users: [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: false,
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false,
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false,
+    },
+  ],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE_USER':
+      return produce(state, (draft) => {
+        draft.users.push(action.user);
+      });
+    case 'TOGGLE_USER':
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === action.id);
+        user.active = !user.active;
+      });
+    case 'REMOVE_USER':
+      return produce(state, (draft) => {
+        const index = draft.users.findIndex((user) => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
+    default:
+      return state;
+  }
+}
+
+export const UserDispatch = React.createContext(null);
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { users } = state;
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser />
+      <UserList users={users} />
+      <div>활성사용자 수 : {count}</div>
+    </UserDispatch.Provider>
   );
 }
 
