b27eec666988d99feabc16adf5815dac64e1c934
lazarus0320 <astar5327z@gmail.com>
2023-02-18 12:00:31 +0900

    23/02/18

--- pokemon-wiki/src/index.js
+++ pokemon-wiki/src/index.js
@@ -1,18 +1,14 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import React from 'react';
-import { BrowserRouter as Router, Outlet } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = document.getElementById('root');
+
+ReactDOM.createRoot(root).render(
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
 );
-
 reportWebVitals();
